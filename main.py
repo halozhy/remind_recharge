4f366366fa4a8342f88ab1f7668b2a3b57a110ed
halozhy <halozhy@qq.com>
2021-06-30 17:20:06 +0800

    加入try

--- main.py
+++ main.py
@@ -15,33 +15,36 @@ def get_card_money(username, password):
     session = requests.Session()
     urllib3.disable_warnings()
     url_pass_neu = "https://pass.neu.edu.cn/tpass/login?service=https%3A%2F%2Fportal.neu.edu.cn%2Ftp_up%2F"
-    r = session.get(url_pass_neu)
-    lt_list = re.compile(r'name="lt" value="(.+?)"').findall(r.text)
-    lp_list = re.compile(r'id="loginForm" action="(.+?)"').findall(r.text)
-    if len(lt_list) != 1 or len(lp_list) != 1:
-        print('bad get neu login page request')
-        exit(0)
-    lt = lt_list[0]
-    login_path = lp_list[0]
-    login_body = f'rsa={username}{password}{lt}&ul={len(username)}&pl={len(password)}&lt={lt}' \
-                 '&execution=e1s1&_eventId=submit'
-    login_header: dict = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Host': 'pass.neu.edu.cn',
-        'Origin': 'https://pass.neu.edu.cn',
-    }
-    r = session.post(url=f'https://pass.neu.edu.cn{login_path}', data=login_body, headers=login_header,
-                     allow_redirects=False)
-    if not r.headers.__contains__("Location"):
-        print("bad login neu request, please check your ID or PWD")
-        exit(0)
-    session.get(url=r.headers["Location"], allow_redirects=False)
-    r = session.post("https://portal.neu.edu.cn/tp_up/up/subgroup/getCardMoney", data={})
-    r_dict = json.loads(r.text)
-    if not r_dict.__contains__("card_balance"):
-        print("bad get card balance")
-        exit(0)
-    return float(r_dict["card_balance"]) / 100
+    try:
+        r = session.get(url_pass_neu)
+        lt_list = re.compile(r'name="lt" value="(.+?)"').findall(r.text)
+        lp_list = re.compile(r'id="loginForm" action="(.+?)"').findall(r.text)
+        if len(lt_list) != 1 or len(lp_list) != 1:
+            print('bad get neu login page request')
+            exit(0)
+        lt = lt_list[0]
+        login_path = lp_list[0]
+        login_body = f'rsa={username}{password}{lt}&ul={len(username)}&pl={len(password)}&lt={lt}' \
+                     '&execution=e1s1&_eventId=submit'
+        login_header: dict = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Host': 'pass.neu.edu.cn',
+            'Origin': 'https://pass.neu.edu.cn',
+        }
+        r = session.post(url=f'https://pass.neu.edu.cn{login_path}', data=login_body, headers=login_header,
+                         allow_redirects=False)
+        if not r.headers.__contains__("Location"):
+            print("bad login neu request, please check your ID or PWD")
+            exit(0)
+        session.get(url=r.headers["Location"], allow_redirects=False)
+        r = session.post("https://portal.neu.edu.cn/tp_up/up/subgroup/getCardMoney", data={})
+        r_dict = json.loads(r.text)
+        if not r_dict.__contains__("card_balance"):
+            print("bad get card balance")
+            exit(0)
+        return float(r_dict["card_balance"]) / 100
+    except Exception as e:
+        print(e)
 
 
 def send_warn_email(mail_host, mail_user, mail_pass, mail_receiver, subject, content):
